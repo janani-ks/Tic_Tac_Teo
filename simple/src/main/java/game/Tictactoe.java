f84bb6a08fb18397d81403869753a21187412ac9
janani-ks <123439325+janani-ks@users.noreply.github.com>
2023-02-08 15:16:52 +0530

    Update Tictactoe.java

--- simple/src/main/java/game/Tictactoe.java
+++ simple/src/main/java/game/Tictactoe.java
@@ -3,11 +3,9 @@ import java.io.FileDescriptor;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
-class Game{
+class Tac{
 	PrintStream p=new PrintStream((new FileOutputStream(FileDescriptor.out)));
-	boolean horizontal(char c,int u,int v,char[][] arr) {
-		int x = u;
-		int y = v;
+	boolean horizontal(char c,int x,int y,char[][] arr) {
 		for(int i=0;i<x;i++) {
 			if(arr[i][y]!=c) {
 				return false;
@@ -20,9 +18,7 @@ class Game{
 		}
 		return true;
 	}
-	boolean vertical(char c,int u,int v,char[][] arr) {
-		int x = u;
-		int y = v;
+	boolean vertical(char c,int x,int y,char[][] arr) {
 		for(int i=0;i<y;i++) {
 			if(arr[x][i]!=c) {
 				return false;
@@ -35,9 +31,9 @@ class Game{
 		}
 		return true;
 	}
-	boolean side(char c,int u,int v,char[][] arr) {
-		if(v==arr.length-1) {
-			for(int i=v;i>=0;i--) {
+	boolean side(char c,int x,int y,char[][] arr) {
+		if(y==arr.length-1) {
+			for(int i=y;i>=0;i--) {
 				if(arr[i][i]!=c) {
 					return false;
 				}
@@ -52,18 +48,17 @@ class Game{
 		}
 		return true;
 	}
-	boolean side1(char c,int u,int v,char[][] arr) {
-		if(v==arr.length-1) {
-			int j = 1;
-			for(int i=v-1;i>=0;i--) {
+	boolean side1(char c,int x,int y,char[][] arr) {
+		int j = 1;
+		if(y==arr.length-1) {
+			for(int i=y-1;i>=0;i--) {
 				if(arr[j++][i]!=c) {
 					return false;
 				}
 			}
 		}
 		else {
-			int j = 1;
-			for(int i=u-1;i>=0;i--) {
+			for(int i=x-1;i>=0;i--) {
 				if(arr[i][j++]!=c) {
 					return false;
 				}
@@ -80,69 +75,59 @@ class Game{
 		}
 	}
 }
-public class Tictactoe {
+public class Tic {
+	static Scanner s = new Scanner(System.in);
+	static int u =0;
+	static int v =0;
+	static int p1 = 0;
+	static void store(int[] pos) {
+		p1 = s.nextInt();
+		if(pos[p1-1] <=9) {
+			u = 0;
+			v = pos[p1-1];
+		}
+		else
+		{
+			u = pos[p1-1]/10;
+			v = pos[p1-1]%10;
+		}
+	}
 	public static void main(String[] arg) {
-		Scanner s = new Scanner(System.in);
-		Game g = new Game();
+		Tac g = new Tac();
 		g.p.println("Enter the dimention ");
 		int d = s.nextInt();
 		int [] pos = new int[d*d];
+		char [][] arr = new char[d][d];
 		int k=0;
-		for(int i=0;i<d;i++) {
+		int i = 0;
+		boolean b =false;
+		char x = 0;
+		for(i=0;i<d;i++) {
 			for(int j=0;j<d;j++) {
 				pos[k++] = i*10+j;
 			}
 		}
-		char [][] arr = new char[d][d];
-		char x = 0;
-		int i = 0;
-		int p1 = 0;
-		int u =0;
-		int v =0;
-		boolean b =false;
 		g.p.println("Player 1 uses Symbol 'X' ");
 		g.p.println("Player 2 uses Symbol 'O' ");
+		i=0;
 	    while(i<d*d){
 				if(i%2==0){
 					g.p.println("1st Player's turn!!");
-					g.p.println("Enter the position to place");	
-					p1 = s.nextInt();
-					if(pos[p1-1] <=9) {
-						u = 0;
-						v = pos[p1-1];
-					}
-					else
-					{
-						u = pos[p1-1]/10;
-						v = pos[p1-1]%10;
-					}
-					if(arr[u][v]==0) {
-					      arr[u][v] = 'X';
-                          x = 'X';
-					}
-					else
-						g.p.println("Already placed position Enter anyother position!!!");	
+					x = 'X';
 				}
 				else {
 					g.p.println("2nd Player's turn!!");
-					g.p.println("Enter the position to place");	
-					p1 = s.nextInt();
-					if(pos[p1-1]<=9) {
-						u = 0;
-						v = pos[p1-1];
-					}
-					else
-					{
-						u = pos[p1-1]/10;
-						v = pos[p1-1]%10;
-					}
-					if(arr[u][v]==0) {
-						arr[u][v] = 'O';
-						x = 'O';
-					}
-					else
-						g.p.println("Already placed position Enter anyother position!!!");	
-			    }
+					x = 'O';
+				}
+				g.p.println("Enter the position to place");	
+				store(pos);
+				if(arr[u][v]==0) {
+					arr[u][v] = x;
+				}
+				else {
+					g.p.println("Already placed position Enter anyother position!!!");
+					store(pos);
+				}
 				if(g.horizontal(x,u,v,arr)|| g.vertical(x, u,v,arr)) {
 				b = true;
 				}
